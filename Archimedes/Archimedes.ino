21cc5f93a78b01d39eeb460402efcf2294617c7a
Justin Bull <me@justinbull.ca>
2012-09-29 18:56:17 -0400

    Initial commit

---
+++ Archimedes/Archimedes.ino
@@ -0,0 +1,69 @@
+#include <AFMotor.h>
+#define SAME_SPEED -1
+
+AF_DCMotor rightMotor(1);
+AF_DCMotor leftMotor(2);
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Motor test!");
+  
+  rightMotor.setSpeed(100);     // set the speed to 200/255
+  leftMotor.setSpeed(100);     // set the speed to 200/255
+}
+
+void setRobotSpeed(int speed) {
+  if(0 <= speed <= 255) {
+    rightMotor.setSpeed(speed);     // set the speed to 200/255
+    leftMotor.setSpeed(speed);     // set the speed to 200/255     
+  }
+}
+void goForward(int robotSpeed) {
+ setRobotSpeed(robotSpeed);
+ leftMotor.run(FORWARD); 
+ rightMotor.run(FORWARD); 
+
+}
+void goBackward(int robotSpeed) {
+ setRobotSpeed(robotSpeed);
+ leftMotor.run(BACKWARD); 
+ rightMotor.run(BACKWARD); 
+}
+void turnLeft(int turnSpeed) {
+ setRobotSpeed(turnSpeed);
+ leftMotor.run(BACKWARD); 
+ rightMotor.run(FORWARD); 
+}
+void turnRight(int turnSpeed) {
+ setRobotSpeed(turnSpeed);
+ leftMotor.run(FORWARD); 
+ rightMotor.run(BACKWARD); 
+}
+void goRelease() {
+ leftMotor.run(RELEASE); 
+ rightMotor.run(RELEASE); 
+}
+
+
+void loop() {
+  Serial.println("forward");
+  
+  goForward(SAME_SPEED);
+  delay(2000);
+
+  Serial.println("backward");
+  goBackward(SAME_SPEED);
+  delay(2000);
+  
+  Serial.println("turn left");
+  turnLeft(SAME_SPEED);
+  delay(2000);
+  
+  Serial.println("turn right");
+  turnRight(SAME_SPEED);
+  delay(2000);
+  
+  Serial.println("released motors");
+  goRelease();
+  delay(2000);
+}
--- README.md
+++ README.md
@@ -2,3 +2,6 @@ archimedes-robot
 ================
 
 The logic behind the team robot for CPS 607
+
+## Requirements
+1. Adafruit Motor Driver libraries
